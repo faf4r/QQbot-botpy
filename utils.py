66ef1c585d6ed2c896fce42d58f83516476d5537
faf4r <edgecage@foxmail.com>
2025-03-14 18:17:40 +0800

    chore: 修改外联跳转设置

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from urllib.parse import quote
 from botpy.ext.cog_yaml import read
 
 config = read("config.yaml")
@@ -6,5 +7,6 @@ config = read("config.yaml")
 # 鉴于QQ机器人的限制，无法直接发送链接，因此用备案过的链接进行重定向
 def redirect(url):
     redirect_url = config["redirect_url"]   # 备案过的域名，提供重定向服务
-    return redirect_url + url
-
+    if redirect_url:
+        return redirect_url.format(quote(url))
+    return url
